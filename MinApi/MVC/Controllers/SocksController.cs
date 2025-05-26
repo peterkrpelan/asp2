10ba20b8ccd937dce3a099539babe714448b6118
Peter <peter.krpelan@gmail.com>
2025-05-26 15:06:54 +0200

    GetByID

---
+++ MinApi/MVC/Controllers/SocksController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class SocksController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = SockDataSet.GetSocks();
+            return View(data);
+        }
+        public IActionResult GetByID(int id)
+        {
+            var data = SockDataSet.GetSocks().Where(x => x.Id == id).FirstOrDefault();
+            return PartialView(data);
+        } 
+    }
+}
---
+++ MinApi/MVC/Models/Socks.cs
@@ -0,0 +1,34 @@
+﻿namespace MVC.Models
+{
+    public class Socks
+    {
+        public int Id { get; set; }
+        public string? Brand { get; set; }
+        public SockSize Size { get; set; }
+
+        public decimal Price { get; set; }
+        public int OnStock {  get; set; }
+    }
+
+    public enum SockSize
+    {
+        S, M, L
+    }
+    public class SockDataSet
+    {
+        static string[] brands = ["icebreaker", "adidas", "reebook", "puma"];
+        public static IEnumerable<Socks> GetSocks()
+        {
+            var _socks = Enumerable.Range(1, 5).Select(index =>
+            new Socks
+            {
+                Id = index,
+                Brand = brands[Random.Shared.Next(brands.Length)],
+                Size = (SockSize)Random.Shared.Next(4),
+                Price = (decimal)Random.Shared.Next(50, 500),
+                OnStock = Random.Shared.Next(10, 100)
+            }).ToArray();
+            return _socks;
+        }
+    }
+}
