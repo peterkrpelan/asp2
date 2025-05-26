44022b560927805c37074871f85adc8eabd79b94
Peter <peter.krpelan@gmail.com>
2025-05-26 16:19:00 +0200

    MVC Uloha

---
+++ MinApi/MVC/Controllers/APIController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    [Route("ponozky")]
+    [ApiController]
+    public class APIController : ControllerBase
+    {
+        [Route("[action]/{id}")]
+        public Socks? GetById(int id)
+        {
+            var data = SockDataSet.GetSocks().Where(x => x.Id == id).FirstOrDefault();
+            return data;
+        }
+    }
+}
--- MinApi/MVC/Controllers/SocksController.cs
+++ MinApi/MVC/Controllers/SocksController.cs
@@ -3,17 +3,28 @@ using MVC.Models;
 
 namespace MVC.Controllers
 {
+    [Route("[controller]")]
     public class SocksController : Controller
     {
+        [Route("[action]")]
         public IActionResult Index()
         {
             var data = SockDataSet.GetSocks();
             return View(data);
         }
+        [Route("[action]")]
         public IActionResult GetByID(int id)
         {
             var data = SockDataSet.GetSocks().Where(x => x.Id == id).FirstOrDefault();
             return PartialView(data);
-        } 
+        }
+        [Route("SearchByPrice/min/{minPrice:int}/max/{maxprice}")]
+        public IActionResult SearchByPrice(int minPrice, int maxprice)
+        {
+            var data = SockDataSet.GetSocks().Where(x => x.Price >= minPrice && x.Price <= maxprice);
+            return View("Index", data);
+
+        }
+
     }
 }
